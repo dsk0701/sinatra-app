bf33eef04689ef7d0ab52206777a9573e453cabe
dsk0701 <d.dubandtribal@gmail.com>
2017-08-04 22:12:42 +0900

    Change the color during the hover.

--- public/lgtm/js/script.js
+++ public/lgtm/js/script.js
@@ -6,3 +6,26 @@ $(document).on('change', ':file', function() {
     $('#input-text-file').val(label)
 });
 
+var dropArea = $('#drag-drop-area')
+dropArea.on('drop',function(evt){
+    evt.preventDefault();
+    dropArea.removeClass('drag-drop-outside-over')
+    console.log(evt.originalEvent.dataTransfer.files);
+});
+
+dropArea.on('dragover',function(evt){
+    evt.preventDefault();
+    dropArea.addClass('drag-drop-outside-over')
+});
+
+dropArea.on('dragenter',function(evt){
+    evt.preventDefault();
+    console.log("dragenter");
+});
+
+dropArea.on('dragleave',function(evt){
+    evt.preventDefault();
+    dropArea.removeClass('drag-drop-outside-over')
+    console.log("dragleave");
+});
+
