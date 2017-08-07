956ae990b7d594706b0039ff7c7b15fd43b93ac8
dsk0701 <d.dubandtribal@gmail.com>
2017-08-07 09:10:53 +0900

    Implementing submit process.

--- public/lgtm/js/script.js
+++ public/lgtm/js/script.js
@@ -7,7 +7,7 @@ $(document).on('change', ':file', function() {
 });
 
 var dropArea = $('#drag-drop-area')
-dropArea.on('drop',function(evt){
+dropArea.on('drop',function(evt) {
     evt.preventDefault();
     dropArea.removeClass('drag-drop-outside-over')
 
@@ -15,19 +15,52 @@ dropArea.on('drop',function(evt){
     $('#input-text-file').val(file.name)
 });
 
-dropArea.on('dragover',function(evt){
+dropArea.on('dragover',function(evt) {
     evt.preventDefault();
     dropArea.addClass('drag-drop-outside-over')
 });
 
-dropArea.on('dragenter',function(evt){
+dropArea.on('dragenter',function(evt) {
     evt.preventDefault();
     console.log("dragenter");
 });
 
-dropArea.on('dragleave',function(evt){
+dropArea.on('dragleave',function(evt) {
     evt.preventDefault();
     dropArea.removeClass('drag-drop-outside-over')
     console.log("dragleave");
+});
+
+// $('#button-upload').on('click',function(evt) {
+//     console.log("button-upload::onclick");
+//     // TODO: Call upload api and update screen.
+// 
+//     evt.preventDefault();
+//     $(this.form).submit();
+//     $(this.form).find("textarea, :text, select").val("");
+//     return false;
+// });
+
+$('#form-upload').on('submit',function(evt) {
+    console.log("form-upload::submit()");
+    evt.preventDefault();
+
+    $.ajax({
+        url: 'http://localhost:4567/upload',
+        type:'POST',
+        // dataType: 'jsonp',
+        data : { key : 'value' },
+        timeout:10000,
+        success: function(data) {
+            // TODO: 成功表示する
+            console.log("upload success");
+            alert("ok");
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            // TODO: エラー表示する
+            console.log("upload failed");
+            alert("ng");
+        }
+    });
 });
 
